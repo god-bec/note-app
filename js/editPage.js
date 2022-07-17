6e98509d8e5c2402f5674ee40e3d68e8d1faf231
betty <bettybutter@btech.int>
2022-07-17 21:07:11 +0100

    second commit

--- js/editPage.js
+++ js/editPage.js
@@ -1,23 +1,28 @@
 
 //pushing user note in editPage to display them in the index page using POST method
-const savebtn = document.querySelector('#save_btn')
+const savebtn = document.querySelector('.save_btn');
 savebtn.addEventListener('click', function(){
 const form = document.querySelector('.edit_container');
+const date = new Date();
+const stringDate =date.toDateString();
+const currentTime = `${date.getHours()}:${date.getMinutes()}`;
+let dateSuffix = ' ';
+if(date.getHours() >= 12){
+    dateSuffix +=`PM`;
+}else{dateSuffix += `AM`};
 const notes =
     {
     title: form.title.value,
-    content: form.content.value
+    content: form.content.value,
+    date: `${currentTime} ${dateSuffix}`
     }
      const data = 'http://localhost:3000/noteA';
 fetch(data,{
     method:'POST',
     body: JSON.stringify(notes),
     headers: {"Content-Type":"application/json"}
-});
-window.location.replace('/index.html')
-
-});
-
+}).then(()=>{window.location.replace('/index.html')}).catch((error)=>{console.log("error")});
+})
 
 
 
--- js/index.js
+++ js/index.js
@@ -1,43 +1,56 @@
 
 const body = document.querySelector('.body');
 const textArea = document.getElementById('text-area');
-const conta =document.querySelector('.container')
-const saveButton = document.querySelector('#save_btn')
+const conta = document.querySelector('.container')
 const plus_btn = document.querySelector('.plus')
-const editPage = document.querySelector('.container_editpage')
+const searchForm = document.querySelector('.search-form');
 
-let noteData = "http://localhost:3000/noteA";
-let note = [];
-//fetching json server
-fetch(noteData)
-.then((response)=>{
-    return response.json()
-}).then((data)=>{
-     note.push(...data)
-     console.log(data)
+
+
 //displaying element gotten from json server array,so it can be edited by the user in the text area in index page
-  function fetchData(){
-        const getData = note.map(function(el){
-            return `
-            <form class="note-container">
-            <input name="title" placeholder="${el.title}" class="title"/>
-            <textarea class="text" placeholder="${el.content}"></textarea>
-            </form>`
-        });
-        const textArea = document.querySelector('.container');
-        textArea.innerHTML = getData.join("");
-   
+function fetchData(searchTerm) {
+  let noteData = "http://localhost:3000/noteA?_";
+  //this statement fires for only the search input
+  if(searchTerm){
+    noteData +=`&q=${searchTerm}`;
   }
-  window.addEventListener('DOMContentLoaded',fetchData());
+  let note = [];
+  //fetching json server
+  fetch(noteData)
+    .then((response) => {
+      return response.json()
+    }).then((data) => {
+      note.push(...data)
+      const getData = note.map(function (el) {
+        return `
+            <a href="deletePage.html?id=${el.id}" class="js-link"><form class="note-container">
+            <input name="title" placeholder="${el.title}" class="title" disabled/>
+            <textarea class="text" disabled>${el.content}</textarea>
+            <h5>${el.date}<h5>
+            </form>
+            </a>`
+           
+      });
+      const textArea = document.querySelector('.container');
+      textArea.innerHTML = getData.join("");
+    }
+
+    ).catch((error) => { console.log('error') })
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+  fetchData();
+});
+
+//search field code
+
+searchForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+ fetchData(searchForm.search.value.trim());
   
- 
-}).catch((error)=>{console.log('error')});
-
-   
 
 
-
-
+});
 
 
 
