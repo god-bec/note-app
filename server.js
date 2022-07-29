bf97f7ef653284635e964ef05a699e7fe8277a15
betty <bettybutter@btech.int>
2022-07-29 21:24:13 +0100

    server repair 1

--- server.js
+++ server.js
@@ -1,32 +1,23 @@
-// const express = require("express");
-// const app = express();
-// app.get("/", function(req,res){
-//     res.sendFile(__dirname + "/index.html");
-// });
-// app.use(express.static(__dirname));
-// app.listen(3000, function(){
-//     console.log("server is running on localhost3000")
-// });
+let port = process.env.PORT;
+if(req.url === '/') {
+  res.setHeader('Content-Type', 'text/html');
+  res.write(index);
+}
 
+if (port == null || port == "") {
+port = 8000;
+}
 
 const express = require("express");
 const app = express();
-app.listen(3000, () => {
-  console.log("Application started and Listening on port 3000");
-});
-
+// app.listen(3000, () => {
+//   console.log("Application started and Listening on port 3000");
+// });
+app.listen(port);
 // serve your css as static
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-if(req.url === '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write(index);
-  }
-  let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
+
