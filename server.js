dfe3262c76b36844dda350201bbe542917d2cb55
betty <bettybutter@btech.int>
2022-08-14 06:42:15 +0100

    herku commit

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ if(req.url === '/') {
   res.write(index);
 }
 
+app.listen(3000, () => {
+  console.log("Application started and Listening on port 3000");
+});
+app.listen(port);
 if (port == null || port == "") {
-port = 8000;
-}
-
+  port = 8000;
+  }
 const express = require("express");
 const app = express();
-// app.listen(3000, () => {
-//   console.log("Application started and Listening on port 3000");
-// });
-app.listen(port);
+
+
 // serve your css as static
 app.use(express.static(__dirname));
 
