98fe85694bde44bf922665867e8247b5ec404af8
betty <bettybutter@btech.int>
2022-08-15 14:51:08 +0100

    i'm tired commit

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname, "build", "index.html");
+  res.sendFile(__dirname,"index.html");
 });
 
